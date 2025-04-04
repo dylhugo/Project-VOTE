9020ea031caa9ebfcacbb9d5e19a911537c33b28
Dylanhughes5150 <dylan.markjovihughes@aol.com>
2025-04-04 11:07:27 +0100

    updates to content.view
    main page front end all upodated.

--- Project VOTE/ContentView.swift
+++ Project VOTE/ContentView.swift
@@ -7,60 +7,71 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var codeEntered: String = ""
     var body: some View {
-
-        Color(.white)
-            .ignoresSafeArea()
         
         
-//Stack for main text
-        VStack {
-            Text("please enter confimation code's below")
-            .font(.title)
-            .font(.system(size: 20, weight: .bold, design: .default))
-            .multilineTextAlignment(.center)
-        
-        Spacer()
-        }
-
- // stact for code entry
-        VStack {
-          Text("Enter Registration Code")
-            .font(.system(size: 20, weight: .bold, design: .default))
-                .multilineTextAlignment(.center)
             
-            TextField("", text: .constant(""))
-                .padding()
-                .frame(width: 300, height: 50)
-                .keyboardType(.numberPad)
-                .background(Color(.gray).opacity(0.2))
-                .cornerRadius(10)
-        
-        Spacer()
             
-            Text("Enter 6-didget Code")
-              .font(.system(size: 20, weight: .bold, design: .default))
-                  .multilineTextAlignment(.center)
-              
-              TextField("", text: .constant(""))
-                  .padding()
-                  .frame(width: 300, height: 50)
-                  .keyboardType(.numberPad)
-                  .background(Color(.gray).opacity(0.2))
-                  .cornerRadius(10)
-         
-        Spacer()
             
-            Button("Confirm") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            
+            
+            //Stack for main text
+            VStack {
+                Spacer()
+                
+                Text("please enter confimation code's below")
+                    .font(.title)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
             }
-        Spacer()
-        }
+            
+            // stact for code entry
+            VStack {
+                Text("Enter Registration Code")
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .multilineTextAlignment(.center)
+                
+                TextField("", text: .constant(""))
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .keyboardType(.numberPad)
+                    .background(Color(.gray).opacity(0.2))
+                    .cornerRadius(10)
+                
+                Spacer()
+                
+                Text("Enter 6 digit code")
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .multilineTextAlignment(.center)
+                
+                SecureField("6 didget code", text: $codeEntered)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .keyboardType(.numberPad)
+                    .background(Color(.gray).opacity(0.2))
+                    .cornerRadius(10)
+                    .autocorrectionDisabled(true)
+                
+                
+                
+                Spacer()
+                
+                Button("Confirm") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                Spacer()
+            }
+            
+            
+            
+            // end
         
-        
-
-// end
     }
 }
 
